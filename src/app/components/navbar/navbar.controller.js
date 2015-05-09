3f5de5db552a994c8ef4ab526ad90ac84a103d7b
Sugan Krishnan <sugan_gopalakrishnan@external.mckinsey.com>
2015-05-09 09:45:51 +0530

    fix jshint issues

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('fdsfrontEnd')
-  .controller('NavbarCtrl', function ($scope, $window) {
+  .controller('NavbarCtrl', function ($scope) {
     $scope.currentProject = localStorage.getItem('project');
   });
--- src/app/index.controller.js
+++ src/app/index.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('fdsfrontEnd')
-  .controller('IndexCtrl', function ($scope) {
+  .controller('IndexCtrl', function () {
 
   });
